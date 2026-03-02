9ddf66cffdee440ba450a0aa9cd4454b7820adce
Kevin Mercado <kevinmer@192.168.1.71>
2026-03-01 23:38:51 -0600

    Actualizando diseño

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React kevvvvvv JAJAJ</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <nav className="navbar">
+        <h2 className="logo">KM</h2>
+        <ul className="nav-links">
+          <li>Inicio</li>
+          <li>Proyectos</li>
+          <li>Sobre mí</li>
+          <li>Contacto</li>
+        </ul>
+      </nav>
+
+      <section className="hero">
+        <div className="hero-left">
+          <p className="intro">Hola, soy</p>
+          <h1 className="name">Kevin Meraz</h1>
+          <h2 className="role">Frontend Developer</h2>
+
+          <p className="description">
+            Creo experiencias digitales modernas, limpias y funcionales.
+            Especializado en React, JavaScript y diseño UI elegante.
+          </p>
+
+          <div className="buttons">
+            <button className="btn-primary">Ver Proyectos</button>
+            <button className="btn-secondary">Descargar CV</button>
+          </div>
+        </div>
+
+        <div className="hero-right">
+          <div className="image-container">
+            <img src="/foto.jpg" alt="Kevin" />
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
